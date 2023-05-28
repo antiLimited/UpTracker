cf7b0350990b7dce21b2f7c1cf01bf34034b47fd
Interfiber <webmaster@interfiber.dev>
2023-05-28 14:06:38 -0400

    m

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 extern crate uptime_lib;
 
 fn main() {
+
+    // Constants
     let minute: f64 = 60.0;
     let day: f64 = 24.0;
-    match uptime_lib::get() {
-        Ok(utime) => {
-            let uptime = uptime.as_secs_f64();
-            let hours = ((uptime/ minute / minute) - ((uptime / minute / minute / day).floor() * day)).floor();
-            let mins = ((uptime / minute) - ((uptime / minute / minute).floor() * minute)).floor();
-            let seconds = ((uptime) - ((uptime / minute).floor() * minute)).floor();
-            let days = (uptime / minute / minute / day).floor();
+
+    // Raw uptime
+    let utime = uptime_lib::get().expect("Failed to aquire uptime");
+
+    // 64 bit float version uptime
+    let uptime = uptime.as_secs_f64();
 
 
-            print!("Uptime: {} days,", days);
-            print!(" {} hours,", hours);
-            print!(" {} minutes,", mins);
-            println!(" {} seconds", seconds);
-        }
-        Err(err) => {
-            eprintln!("uptime: {}", err);
-            std::process::exit(1);
-        }
-    }
+    let hours =
+        ((uptime / minute / minute) - ((uptime / minute / minute / day).floor() * day)).floor();
+    let mins = ((uptime / minute) - ((uptime / minute / minute).floor() * minute)).floor();
+    let seconds = ((uptime) - ((uptime / minute).floor() * minute)).floor();
+    let days = (uptime / minute / minute / day).floor();
+
+    print!("Uptime: {} days,", days);
+    print!(" {} hours,", hours);
+    print!(" {} minutes,", mins);
+    println!(" {} seconds", seconds);
 }
