9253edbb6a1232a1dde6d316b014e359e08c03c1
antiLimited <antilimitedmail@gmail.com>
2023-05-27 23:31:12 -0400

    uptime_lib working :3

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate uptime_lib;
+
 fn main() {
-    println!("Hello, world!");
+    let minute: f64 = 60.0;
+    match uptime_lib::get() {
+        Ok(uptime) => {
+            print!("Uptime: {} hours,", (uptime.as_secs_f64() / minute / minute).floor());
+            print!(" {} minutes,", ((uptime.as_secs_f64() / minute) - ((uptime.as_secs_f64() / minute / minute).floor() * minute)).floor());
+            println!(" {} seconds", ((uptime.as_secs_f64()) - ((uptime.as_secs_f64() / minute).floor() * minute)).floor());
+        }
+        Err(err) => {
+            eprintln!("uptime: {}", err);
+            std::process::exit(1);
+        }
+    }
 }
