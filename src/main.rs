b356020f211cd156c40cbbebd1e88866d3e7f4e2
antiLimited <antilimitedmail@gmail.com>
2023-05-28 13:44:04 -0400

    added days

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ extern crate uptime_lib;
 
 fn main() {
     let minute: f64 = 60.0;
+    let day: f64 = 24.0;
     match uptime_lib::get() {
         Ok(uptime) => {
-            print!("Uptime: {} hours,", (uptime.as_secs_f64() / minute / minute).floor());
+            print!("Uptime: {} days,", (uptime.as_secs_f64() / minute / minute / day).floor());
+            print!(" {} hours,", ((uptime.as_secs_f64() / minute / minute) - ((uptime.as_secs_f64() / minute / minute / day).floor() * day)).floor());
             print!(" {} minutes,", ((uptime.as_secs_f64() / minute) - ((uptime.as_secs_f64() / minute / minute).floor() * minute)).floor());
             println!(" {} seconds", ((uptime.as_secs_f64()) - ((uptime.as_secs_f64() / minute).floor() * minute)).floor());
         }
